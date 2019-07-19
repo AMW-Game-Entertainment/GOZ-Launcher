dceef52427feaa46246d8c483b7aaeaab5d965a9
Redeemer Pace <digital.2@brndwgn.com>
2019-07-19 10:18:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GOZ-Launcher
+ Game Launcher in eletrcon and vue
---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'goz-gamelauncher'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
