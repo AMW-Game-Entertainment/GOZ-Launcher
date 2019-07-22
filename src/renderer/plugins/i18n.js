4cea7da87df8e27b0e91caa86c5f66cc0891410f
AMW - Game & Entertainment <animemixedworld@gmail.com>
2019-07-23 01:47:46 +0200

    Added i18n

--- src/renderer/plugins/i18n.js
+++ src/renderer/plugins/i18n.js
@@ -1,21 +1,13 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
-import enElement from 'element-ui/lib/locale/lang/en'
-// import zhElement from 'element-ui/lib/locale/lang/zh-CN'
-// import zhTWElement from 'element-ui/lib/locale/lang/zh-TW'
-
 import enUS from '@/assets/translations/en'
-// import zhCN from '../../helper/i18n/zh-CN'
-// import zhTW from '../../helper/i18n/zh-TW'
-
-const lang = require('electron').ipcRenderer.sendSync('request-lang')
 
 Vue.use(VueI18n)
 
 export default new VueI18n({
-  locale: lang,
+  locale: 'en-US',
   messages: {
-    'en-US': Object.assign(enElement, enUS)
+    'en-US': enUS
   }
 })
