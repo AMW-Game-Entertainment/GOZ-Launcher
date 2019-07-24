ff335fbd01d9bf0d8d464795da1e9cd1011a49c7
AMW - Game & Entertainment <animemixedworld@gmail.com>
2019-07-24 21:00:55 +0200

    Fixed store not dispatching

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import { createPersistedState, createSharedMutations } from 'vuex-electron'
+import { createPersistedState } from 'vuex-electron'
 
 import modules from './modules'
 import getters from './getters'
@@ -9,10 +9,11 @@ import getters from './getters'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  namespaced: true,
   modules,
   plugins: [
-    createPersistedState(),
-    createSharedMutations()
+    createPersistedState()
+    // createSharedMutations()
   ],
   getters,
   strict: process.env.NODE_ENV !== 'production'
