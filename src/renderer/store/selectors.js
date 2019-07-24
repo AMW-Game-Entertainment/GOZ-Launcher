82d552331d2622863dd530603c508c9c59e9f0bb
AMW - Game & Entertainment <animemixedworld@gmail.com>
2019-07-24 22:17:12 +0200

    Removed unnecessary selectors

--- src/renderer/store/selectors.js
+++ src/renderer/store/selectors.js
@@ -1,5 +1,3 @@
-export const getTotalOnlineInGame = (state) => state.inGame.totalOnline
-
-export const getTotalOfflineInGame = (state) => state.inGame.totalOffline
-
-export const getConfig = (state, name) => state.config[name]
+export const getConfig = (state, name) => {
+  return state.config[name]
+}
